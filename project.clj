4f22082b6967191fb9292797c5974ebc6cf59424
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-01-17 09:27:34 +0900

    update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject s3anz "0.1.0"
-  :description "FIXME: write description"
+  :description "a tamaki plugin to deploy to s3"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "https://github.com/nryotaro/s3anz"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  ;; https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
